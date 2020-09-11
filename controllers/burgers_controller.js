7cd1035300c00601457b007d11d319e53947a31f
RazzleJazzled <robertjgassner@gmail.com>
2020-09-10 17:45:53 -0700

    setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const router = express.Router();
+
+const burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        const hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res){
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(results) {
+        res.json({ id: results.insertID });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    const condition = "id =" + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
